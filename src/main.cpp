a12188c85387d3eaf3366b36771e2d7932a3a6c0
MrSnort <clovis.marcille@gmail.com>
2025-12-09 21:08:59 +0100

    framebuffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include "eadkpp.h"
+#include "ressources.h"
 #include <stdint.h>
 #include <stdbool.h>
+#include <memory.h>
 
-extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "GAME";
+
+//the function to push a part of the tilemap to a specific location in the framebuffer
+//x,y coordinate in the framebuffer
+//x2,y2 coordinate in the tilemap
+//w,h width and height of part of tilemap to draw
+void PushFramebuffer(uint16_t *fb,uint16_t x, uint16_t y,uint16_t x2,uint16_t y2,uint16_t w,uint16_t h){
+    for (int i = 0; i < /*240-y-*/h; i++){ //add part of the tilemap to framebuffer horizontal line by horizontal line up to bottom
+        memcpy(&fb[(y+i)*320+x] ,&tilemap[(i+y2)*144+x2],(w)*2); //320 is the width of the screen and 144 the width of the tilemap
+    }
+}
+
+//Numworks stuff
+extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "GAME"; //name of the app
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 int main(int argc, char *argv[])
 {
-    EADK::Display::pushRectUniform(EADK::Screen::Rect, 0x922BFF);
-
     bool running = true;
+    eadk_color_t framebuffer[320*240] = {0x07E0};
 
+    //game loop
     while (running)
     {
         EADK::Keyboard::State kbdState = EADK::Keyboard::scan();
         running = !kbdState.keyDown(EADK::Keyboard::Key::Home);
+
+        PushFramebuffer(framebuffer,50,50,32,144,48,48); //push the water tile in the framebuffer
+        eadk_display_wait_for_vblank(); //wait for the beginning of the next frame
+        eadk_display_push_rect(eadk_screen_rect, framebuffer); //push framebuffer to screen
     }
 }
