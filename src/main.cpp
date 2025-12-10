03e39d80a64bbf2ed22e77ddfe8efe9348c3078c
MrSnort <clovis.marcille@gmail.com>
2025-12-10 21:13:26 +0100

    map and player test

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,21 @@ extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_le
 int main(int argc, char *argv[])
 {
     bool running = true;
+    uint16_t x = 0; //player coordinate
+    uint16_t y = 0; //player coordinate
     eadk_color_t framebuffer[320*240] = {0x07E0};
+    uint64_t map[10][10] = {
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1}
+    };
 
     //game loop
     while (running)
@@ -31,7 +45,37 @@ int main(int argc, char *argv[])
         EADK::Keyboard::State kbdState = EADK::Keyboard::scan();
         running = !kbdState.keyDown(EADK::Keyboard::Key::Home);
 
-        PushFramebuffer(framebuffer,50,50,32,144,48,48); //push the water tile in the framebuffer
+        //clear framebuffer
+        memset(framebuffer,0x2945,sizeof(framebuffer));
+
+        //draw map
+        for(uint16_t i = 0;i < 10;i++){
+            for(uint16_t j =0;j < 10;j++){
+                if (map[i][j] == 1){
+                    PushFramebuffer(framebuffer,i*16,j*16,32,16,16,16); // 1 = wall
+                }
+                else{
+                    PushFramebuffer(framebuffer,i*16,j*16,80,0,16,16); // 0 = ground
+                }
+            }
+        }
+
+        //input
+        if (kbdState.keyDown(EADK::Keyboard::Key::Right)){
+            x++;
+        }
+        if (kbdState.keyDown(EADK::Keyboard::Key::Left)){
+            x--;
+        }
+        if (kbdState.keyDown(EADK::Keyboard::Key::Up)){
+            y--;
+        }
+        if (kbdState.keyDown(EADK::Keyboard::Key::Down)){
+            y++;
+        }
+        PushFramebuffer(framebuffer,x,y,80,144,16,16); //player update (torch for test)
+
+        //PushFramebuffer(framebuffer,50,50,32,144,48,48); //push the water tile in the framebuffer
         eadk_display_wait_for_vblank(); //wait for the beginning of the next frame
         eadk_display_push_rect(eadk_screen_rect, framebuffer); //push framebuffer to screen
     }
